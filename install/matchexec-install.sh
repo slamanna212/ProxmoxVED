5434591f5ce3b258e5232badd613e7849cb65947
Slamanna212 <1133401+slamanna212@users.noreply.github.com>
2026-04-30 23:58:04 +0000

    commit changes after running setup fork

--- install/matchexec-install.sh
+++ install/matchexec-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: slamanna212
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/slamanna212/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/slamanna212/matchexec
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
